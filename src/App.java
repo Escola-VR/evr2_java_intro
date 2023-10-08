8110702da1bb2da019058202977e610b889367ec
esdrasilvavr <esdras.silva@vrsoft.com.br>
2023-10-08 10:44:52 -0300

    Ajuste main

--- src/App.java
+++ src/App.java
@@ -1,46 +1,16 @@
-import java.util.Scanner;
-
-import algoritmos.EntradaDeDados;
 import algoritmos.Repeticao;
-import algoritmos.SaidaDeDados;
 
 public class App {
 
     static String global = "Variável Global";
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        // Tipos de dados numericos
-        // Inteiros
-
-        // Para declarar uma variavel em Java, nós utilizamos a sintaxe abaixo.
-        // modificador tipo identificador = valor;
-        // byte mByte = 127;
-        
-
-        // Saída de dados
-        // escreval("Palmeiras ", t1, " X ", t2, "Corinthians")
-        int t1 = 0, t2 = 0;
-        // System.out.println("Palmeiras " + t1 + " X " + t1 + " Corinthians");
-
-        // Entrada de dados:
-        Scanner in = new Scanner(System.in);
-
-        // System.out.println("Digite o placar.");
-        // System.out.print("Palmeiras:");
-        // t1 = in.nextInt(); //leia(t1)
-        // System.out.print("Corinthians:");
-        // t2 = in.nextInt(); //leia(t2)
-
-        // C Style
-        // System.out.printf("Palmeiras %d X %d Corinthians\n", t2, t1);
-
-
-        // EntradaDeDados.executarPlayground();
-        // SaidaDeDados.executarPlayground();
+        /*
+         * Selecione aqui os playgrounds que deseje executar.
+         * Selecione a classe específico.
+         * 
+         * Na próxima aula encerraremos o conteúdo com os sub-algoritmos e os exercícios finais.
+         */
         Repeticao.executarPlayground();
-
-        in.close();
     }
 }
